eb4cd35a320fdde227c6e9517890b5e4d5f11b1c
Andri Deng <andri.deng@qoala.id>
2020-05-03 10:28:16 +0700

    add log

--- src/android/com/andrideng/plugin/HprtPrinter.java
+++ src/android/com/andrideng/plugin/HprtPrinter.java
@@ -19,6 +19,7 @@ import java.util.List;
 import java.util.HashMap;
 import java.util.Iterator;
 
+import android.util.Log;
 import android.app.PendingIntent;
 
 import android.hardware.usb.UsbManager;
